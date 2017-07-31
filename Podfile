479314aab34b391d188b96fdd62c9720b386d9f0
Tancrède Chazallet (AncAinu) <contact@ancainu.com>
2017-07-31 09:39:28 +0100

    Base project

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'tdd-challenge' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for tdd-challenge
+
+  target 'tdd-challengeTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'tdd-challengeUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ tdd-challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tdd-challenge
+//
+//  Created by Tancrède Chazallet on 31/07/2017.
+//  Copyright © 2017 TrueView. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
